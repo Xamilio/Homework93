07a48a3a3d9ebc48bda98b87373149f3385fa39e
Xamilio25 <Xamilio25@XAMILIO>
2024-10-03 19:57:17 +0300

    Добавьте файлы проекта.

---
+++ Homework93/Homework93.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	//1
+	int A = 243;
+	cout << (A >= 100 && A <= 999) << endl;
+
+
+	//2
+	int x = 453;
+	int y = 34;
+	int z = 24;
+	cout << (((x < 45) + (y < 45) + (z < 45)) == 1) << endl;
+
+	//3
+	int A = 56;
+	cout << (-137 <= A <= -51 || 55 <= A && A <= 123);
+
+}
